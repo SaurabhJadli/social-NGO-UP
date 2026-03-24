3b44db836ba0f33e5daf9761b3c66cb3871eaca0
SaurabhJadli <saurabhjadli7@protonmail.com>
2026-03-24 08:19:43 +0530

    fixed footer for mobile and join section added on home

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-    <footer className="hidden md:flex footer bg-base-200 text-base-content p-10 justify-center gap-70">
+    <footer className="hidden md:flex footer bg-base-200 text-base-content p-10 justify-center gap-70 mt-6">
   <nav>
     <h6 className="footer-title">Our Work</h6>
     <a className="link link-hover">Education</a>
@@ -24,56 +24,6 @@ const Footer = () => {
     <a className="link link-hover">Privacy policy</a>
   </nav>
 </footer>
-
-<footer className="block md:hidden footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10">
-  <nav className="grid grid-flow-col gap-4">
-    <a className="link link-hover">About us</a>
-    <a className="link link-hover">Contact</a>
-    <a className="link link-hover">Jobs</a>
-    <a className="link link-hover">Press kit</a>
-  </nav>
-  <nav>
-    <div className="grid grid-flow-col gap-4">
-      <a>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          className="fill-current">
-          <path
-            d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-        </svg>
-      </a>
-      <a>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          className="fill-current">
-          <path
-            d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-        </svg>
-      </a>
-      <a>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          className="fill-current">
-          <path
-            d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-        </svg>
-      </a>
-    </div>
-  </nav>
-  <aside>
-    <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
-  </aside>
-</footer>
-
 
 <footer className="hidden md:block footer bg-base-200 text-base-content border-base-300 border-t px-10 py-4">
   
@@ -117,6 +67,7 @@ const Footer = () => {
       </a>
     </div>
 
+{/* Brand / NGO Logo */}
 <div className='flex'>
     <svg
       width="24"
@@ -137,9 +88,65 @@ const Footer = () => {
   </div>
 
   </nav>
-
-  
 </footer>
+
+{/* Footer for mobile starts here */}
+
+<footer className="block md:hidden footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10 mt-6">
+  <nav className="grid grid-flow-col gap-4">
+    <a className="link link-hover footer-title">About us</a>
+    <a className="link link-hover footer-title">Contact</a>
+    <a className="link link-hover footer-title">Jobs</a>
+    <a className="link link-hover footer-title">Press kit</a>
+  </nav>
+  <nav>
+
+    {/* Social Media Logos */}
+    <h6 className="footer-title m-3">Follow us on</h6>
+    <div className="grid grid-flow-col gap-4">
+      <a>
+        <svg
+            xmlns="http://www.w3.org/2000/svg" 
+            width="30"
+            height="30"
+            viewBox="0 0 640 640">
+                <path fill="rgb(116, 192, 252)" 
+                d="M240 363.3L240 576L356 576L356 363.3L442.5 363.3L460.5 265.5L356 265.5L356 230.9C356 179.2 376.3 159.4 428.7 159.4C445 159.4 458.1 159.8 465.7 160.6L465.7 71.9C451.4 68 416.4 64 396.2 64C289.3 64 240 114.5 240 223.4L240 265.5L174 265.5L174 363.3L240 363.3z"/></svg>
+      </a>
+      <a>
+        <svg
+            xmlns="http://www.w3.org/2000/svg" 
+            width="30"
+            height="30"
+            viewBox="0 0 640 640">
+                <path fill="rgb(255, 38, 38)" 
+                d="M581.7 188.1C575.5 164.4 556.9 145.8 533.4 139.5C490.9 128 320.1 128 320.1 128C320.1 128 149.3 128 106.7 139.5C83.2 145.8 64.7 164.4 58.4 188.1C47 231 47 320.4 47 320.4C47 320.4 47 409.8 58.4 452.7C64.7 476.3 83.2 494.2 106.7 500.5C149.3 512 320.1 512 320.1 512C320.1 512 490.9 512 533.5 500.5C557 494.2 575.5 476.3 581.8 452.7C593.2 409.8 593.2 320.4 593.2 320.4C593.2 320.4 593.2 231 581.8 188.1zM264.2 401.6L264.2 239.2L406.9 320.4L264.2 401.6z"/></svg>
+      </a>
+      <a>
+        <svg 
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30" 
+            viewBox="0 0 640 640">
+                <path fill="rgb(255, 113, 248)" 
+                d="M320.3 205C256.8 204.8 205.2 256.2 205 319.7C204.8 383.2 256.2 434.8 319.7 435C383.2 435.2 434.8 383.8 435 320.3C435.2 256.8 383.8 205.2 320.3 205zM319.7 245.4C360.9 245.2 394.4 278.5 394.6 319.7C394.8 360.9 361.5 394.4 320.3 394.6C279.1 394.8 245.6 361.5 245.4 320.3C245.2 279.1 278.5 245.6 319.7 245.4zM413.1 200.3C413.1 185.5 425.1 173.5 439.9 173.5C454.7 173.5 466.7 185.5 466.7 200.3C466.7 215.1 454.7 227.1 439.9 227.1C425.1 227.1 413.1 215.1 413.1 200.3zM542.8 227.5C541.1 191.6 532.9 159.8 506.6 133.6C480.4 107.4 448.6 99.2 412.7 97.4C375.7 95.3 264.8 95.3 227.8 97.4C192 99.1 160.2 107.3 133.9 133.5C107.6 159.7 99.5 191.5 97.7 227.4C95.6 264.4 95.6 375.3 97.7 412.3C99.4 448.2 107.6 480 133.9 506.2C160.2 532.4 191.9 540.6 227.8 542.4C264.8 544.5 375.7 544.5 412.7 542.4C448.6 540.7 480.4 532.5 506.6 506.2C532.8 480 541 448.2 542.8 412.3C544.9 375.3 544.9 264.5 542.8 227.5zM495 452C487.2 471.6 472.1 486.7 452.4 494.6C422.9 506.3 352.9 503.6 320.3 503.6C287.7 503.6 217.6 506.2 188.2 494.6C168.6 486.8 153.5 471.7 145.6 452C133.9 422.5 136.6 352.5 136.6 319.9C136.6 287.3 134 217.2 145.6 187.8C153.4 168.2 168.5 153.1 188.2 145.2C217.7 133.5 287.7 136.2 320.3 136.2C352.9 136.2 423 133.6 452.4 145.2C472 153 487.1 168.1 495 187.8C506.7 217.3 504 287.3 504 319.9C504 352.5 506.7 422.6 495 452z"/></svg>
+      </a>
+
+      <a href="">
+        <svg xmlns="http://www.w3.org/2000/svg" 
+            width="30"
+            height="30" 
+            viewBox="0 0 640 640">
+                <path d="M453.2 112L523.8 112L369.6 288.2L551 528L409 528L297.7 382.6L170.5 528L99.8 528L264.7 339.5L90.8 112L236.4 112L336.9 244.9L453.2 112zM428.4 485.8L467.5 485.8L215.1 152L173.1 152L428.4 485.8z"/></svg>
+      </a>
+    </div>
+  </nav>
+
+    <p className='mt-4'>Copyright © {new Date().getFullYear()} - All right reserved by ACME Ind. Ltd</p>
+
+</footer>
+
+{/* Footer for mobile ends here */}
     </>
   )
 }
